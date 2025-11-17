f3fcd83785c0ab4a469895d8a7320c8d67bbbd0c
rizonahmed <rizonahmed0486@gmail.com>
2025-11-18 03:14:02 +0600

    completed the problem solving

---
+++ solution.ts
@@ -0,0 +1,99 @@
+// Problem 01 
+const formatValue = (input: string | number | boolean): (string | number | boolean) => {
+
+    if (typeof input === "string") {
+        return input.toUpperCase()
+    } else if (typeof input === "number") {
+        return input * 10
+    } else {
+        return !input
+    }
+
+}
+
+
+// Problem 02
+const getLength = (input: string | any[]): number => {
+    if (typeof input === "string" || Array.isArray(input)) { return input.length }
+    return 0
+}
+
+
+
+// Problem 03
+class Person {
+    name: string;
+    age: number;
+
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    getDetails(): string {
+        return `Name:${this.name}, Age: ${this.age}`;
+    }
+}
+
+
+
+// Problem 04
+const filterByRating = (input: { title: string, rating: number }[]): ({ title: string, rating: number }[]) => {
+    return input.filter(item => item.rating >= 4.0)
+}
+
+
+
+// Problem 05:
+const filterActiveUsers = (input: { id: number, name: string, email: string, isActive: boolean }[]): { id: number, name: string, email: string, isActive: boolean }[] => {
+    return input.filter(activeUser => activeUser?.isActive === true)
+}
+
+
+
+
+// Problem 06:
+interface Book {
+    title: string;
+    author: string;
+    publishedYear: number;
+    isAvailable: boolean;
+}
+
+const printBookDetails = (inputValue: Book): string => {
+    const availabilityStatus = inputValue.isAvailable ? 'Yes' : 'No';
+    return `Title: ${inputValue.title}, Author: ${inputValue.author}, Published: ${inputValue.publishedYear}, Available: ${availabilityStatus}`;
+}
+
+
+
+// Problem 07:
+const getUniqueValues = (value01: (string | number)[], value02: (string | number)[]): (string | number)[] => {
+    const result: (string | number)[] = [];
+
+    for (const value of value01) {
+        if (!result.includes(value)) result.push(value);
+    }
+    for (const value of value02) {
+        if (!result.includes(value)) result.push(value);
+    }
+    return result;
+}
+
+
+
+
+
+// Problem 08:
+const calculateTotalPrice = (inputValue: { name: string; price: number; quantity: number; discount?: number }[]): number => {
+    if (inputValue.length === 0) return 0;
+
+    return inputValue.map((product) => {
+        let total = product.price * product.quantity;
+        if (product.discount !== undefined) {
+            total -= (total * product.discount) / 100;
+        }
+        return total;
+    }).reduce((sum, item) => sum + item, 0);
+}
